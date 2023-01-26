13c8a9fad5e311c611b126aafff89b3454f5f08f
caseyjramirez <caseyjramirez5@gmail.com>
2023-01-26 16:45:42 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main extends Object {
+
+    public static void main(String[] args) {
+        Student max = new Student("max", 22);
+        PrimarySchoolStudent jimmy = new PrimarySchoolStudent("Jimmy", 22, "Carol");
+
+        System.out.println(max.toString());
+        System.out.println(jimmy.toString());
+
+    }
+}
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+class PrimarySchoolStudent extends Student {
+    private String parentName;
+
+    public PrimarySchoolStudent(String name, int age, String parentName) {
+        super(name, age);
+        this.parentName = parentName;
+    }
+
+    @Override
+    public String toString() {
+        return "PrimarySchoolStudent{" +
+                "parentName='" + parentName + '\'' +
+                "} " + super.toString();
+    }
+}
